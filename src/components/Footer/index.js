2732fbf12189be8fe5f352bb99c53e699de6a653
WanderJunio1226 <wanderportfolio@gmail.com>
2023-03-10 19:51:10 -0300

    atualizacão

---
+++ src/components/Footer/index.js
@@ -0,0 +1,2 @@
+const Footer = () => <div className="limite">Create by Wander Rosa</div>
+export default Footer
---
+++ src/components/Top/index.js
@@ -0,0 +1,12 @@
+import './top.css'
+export function Topo() {
+  return (
+    <section>
+      <div className="limite topo">
+        <button>Home</button>
+        <button>Meus Projetos</button>
+        <button>Dark Mode</button>
+      </div>
+    </section>
+  );
+}
